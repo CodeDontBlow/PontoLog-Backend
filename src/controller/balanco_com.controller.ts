4d5270dad950a2b5c19f2644223681476b765558
henrySilverIX <henrysilver.gold@proton.me>
2025-04-20 18:55:30 -0300

    feat[BE-CALL]: Adicionei as rotas de mes, de ano a ano e de mes a mes

--- src/controller/balanco_com.controller.ts
+++ src/controller/balanco_com.controller.ts
@@ -19,4 +19,30 @@ export default class BalancoComerController{
           const data = await this.repository.getBalancoComercialByYear(Number(year), uf);
           new SuccessResponse(message, data).send(res);
         });
+
+    
+    getBalancoComercialByYearRange = (message: string) =>
+        asyncHandler(async (req, res) => {
+          const { startYear, endYear } = req.params;
+          const uf = req.query.uf as string;
+          const data = await this.repository.getBalancoComercialByYearRange(Number(startYear), Number(endYear), uf);
+          new SuccessResponse(message, data).send(res);
+        });
+    
+    getBalancoComercialByMonth = (message: string) =>
+      asyncHandler(async (req, res) => {
+        const { month } = req.params;
+        const uf = req.query.uf as string;
+        const data = await this.repository.getBalancoComercialByMonth(Number(month), uf);
+        new SuccessResponse(message, data).send(res);
+      });
+
+    
+      getBalancoComercialByMonthRange = (message: string) =>
+        asyncHandler(async (req, res) => {
+          const { startMonth, endMonth } = req.params;
+          const uf = req.query.uf as string;
+          const data = await this.repository.getBalancoComercialByMonthRange(Number(startMonth), Number(endMonth), uf);
+          new SuccessResponse(message, data).send(res);
+        });
 }
--- src/routes/balanco.ts
+++ src/routes/balanco.ts
@@ -6,8 +6,17 @@ const router = express.Router();
 const balancoController = new BalancoComerController();
 
 
-//Pesquisar balanco comercial
+//Pesquisar balanco comercial por ano
 router.get("/balanco_comercial/:year", balancoController.getBalancoComercialByYear("Balanço comercial por ano."));
 
+//Pesquisar balanco comercial pelo mês
+router.get("/balanco_comercial/mes/:month", balancoController.getBalancoComercialByMonth("Balanço comercial por mês."));
+
+//Pesquisar balanco comercial de um ano a outro
+router.get("/balanco_comercial/:startYear/:endYear", balancoController.getBalancoComercialByYearRange("Balanço comercial de um ano a outro."));
+
+
+//Pesquisar balanco comercial de um mês a outro
+router.get("/balanco_comercial/mes/:startMonth/:endMonth", balancoController.getBalancoComercialByMonthRange("Balanço comercial de um ano a outro."));
 
 export default router;
