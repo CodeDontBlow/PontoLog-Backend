93d1a90bdae1bec2c833739c7f531dde4596bc6d
leonardo-dsouza <ubuntu@ip-172-31-11-67.ec2.internal>
2025-05-27 14:34:29 +0000

    config/adicionado configs para o servidor

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-import express from "express";
-import router from "./routes";
-import cors from "cors";
-import "./database";
-import { corsUrl } from "./config";
-import { port } from "./config";
-import { preLoadCache } from "./utils/preLoadCache";
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+import router from './routes';
+import cors from 'cors';
+import './database';
+import { corsUrl, port } from './config';
+import { preLoadCache } from './utils/preLoadCache';
 
 const app = express();
 app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-  preLoadCache()
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/pontolog.hopto.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/pontolog.hopto.org/fullchain.pem'),
+};
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`HTTPS Server listening on port ${port}`);
+  preLoadCache();
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import fatoExportacao from "./fatoExportacao";
 import balanco from "./balanco";
 import express from "express";
 import fatoImportacao from "./fatoImportacao";
+import news from "./news"
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use(dimSh);
 router.use("/importacao", fatoImportacao);
 router.use("/exportacao", fatoExportacao);
 router.use("/balanco", balanco);
+router.use(news)
 
 export default router;
